622ef2de41a8b538f47a017b7a5fbe5705662fac
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-04 23:36:21 -0300

    Add files via upload

---
+++ TempC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+int main()
+{
+setlocale(LC_ALL, "ptb"); // posso comentar aqui?
+float tempF, tempC;
+cout << "Temperatura em Fahrenheit: ";
+cin >> tempF;
+tempC=(tempF-32)/1.8;
+cout << "Temperatura em graus Célsius: " << tempC;
+}
