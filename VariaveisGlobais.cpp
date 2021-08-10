1257e61d07fbabd235ab4162523037581c08bbf4
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-10 01:18:08 -0300

    Add files via upload

---
+++ VariaveisGlobais.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<locale>
+using namespace std;
+
+float num1, num2; // variáveis globais
+void troca();
+void dobra();
+
+int main()
+{
+  cout<<"\nNumero 1: ";
+  cin>>num1;
+  cout<<"\nNumero 2: ";
+  cin>>num2;
+  cout<<"\n\n";
+  cout<<"Inicialmente: "<<num1<<", "<<num2;
+  troca();
+  cout<<"\nDados a chamada de troca (): "<<num1<<", "<<num2;
+  dobra();
+  cout<<"\nDados a chamada de dobra (): "<<num1<<", "<<num2;
+  cout<<"\n\n";
+  system("pause");
+  return 0;
+}
+void troca()
+{
+  // dobra(); embora a chamada da função tenha rodado, está com erro de lógica
+  num1+=num2;
+  num2=num1-num2;
+  num1=num1-num2;
+}
+void dobra()
+{
+  num1*=2;
+  num2*=2;
+}
