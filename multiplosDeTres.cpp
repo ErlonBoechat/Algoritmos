aa4f2c7b971051fc551641afa8830a22acb13f84
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-13 00:09:45 -0300

    Update multiplosDeTres.cpp

--- multiplosDeTres.cpp
+++ multiplosDeTres.cpp
@@ -2,7 +2,7 @@
 #include <locale>
 using namespace std; 
 int main() {
-int num, mult, cont=0;
+int num, mult=0, cont=0; // a variável de teste 'mult' precisa ter valor atribuído para proceder na estrutura de seleção 'if' ao testarmos o algoritmo no C++ Tutor
 for(num=0;num<=9;num++)
 {
 cout<<"digite o "<<num+1<<"º número: ";
