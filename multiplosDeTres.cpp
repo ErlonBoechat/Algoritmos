04388ae62612d62a74872c9bfd72612a926221e4
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-13 00:49:03 -0300

    Update multiplosDeTres.cpp

--- multiplosDeTres.cpp
+++ multiplosDeTres.cpp
@@ -3,13 +3,14 @@
 using namespace std; 
 int main() {
 int num, mult=0, cont=0; // a variável de teste 'mult' precisa ter valor atribuído para proceder na estrutura de seleção 'if' ao testarmos o algoritmo no C++ Tutor
-for(num=0;num<=9;num++)
+for(num=1;num<=10;num++)
 {
-cout<<"digite o "<<num+1<<"º número: ";
+cout<<"digite o "<<num<<"º número: ";
 cin>>mult;
 if(mult%3==0)
 {
     cont++;
+    cout<<"mult------------\n"; // indicador visual dos múltiplos de 3
 }
 }
 cout<<"\nQuantidade de múltiplos de 3: "<<cont;
