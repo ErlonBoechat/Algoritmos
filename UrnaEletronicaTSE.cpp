3a4672401a404bf45ad08b10facfc389b6f2dd22
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-17 18:08:55 -0300

    Update UrnaEletronicaTSE.cpp

--- UrnaEletronicaTSE.cpp
+++ UrnaEletronicaTSE.cpp
@@ -11,24 +11,28 @@ while(num!=0)
 if(num==13)
 {
 cout<<"\nVc votou no Lula";
+cout<<"\n********************";
 lula+=1;
 cont+=1;
 }
 else if(num==39)
 {
 cout<<"\nVc votou no Bozo";
+cout<<"\n********************";
 bozo+=1;
 cont+=1;
 }
 else if (num==1)
 {
 cout<<"\nVc votou em branco";
+cout<<"\n********************";
 brancos+=1;
 cont+=1;
 }
 else
 {
 cout<<"\nVc votou nulo";
+cout<<"\n********************";
 nulos+=1;
 cont+=1;
 }
@@ -36,6 +40,7 @@ cout<<"\nDigite o número do seu candidato: ";
 cin>>num;
 }
 cout<<"\n";
+cout<<"\n********************";
 cout<<"\nLula: "<<lula;
 cout<<"\nBozo: "<<bozo;
 cout<<"\nBrancos: "<<brancos;
