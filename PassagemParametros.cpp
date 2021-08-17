ebbf60adede766cd9fb5bcbc8c9b3e101de795b7
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-16 22:57:07 -0300

    Add files via upload

---
+++ PassagemParametros.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void imprimir(int x, int *y){
+    printf("*x: %d\n", x);
+    printf("*y: %d\n", *(y));
+    x = 10;
+    *(y) = 10;
+}
+
+int main(){
+    int x, y;
+    x = 1;
+    y = 1;
+    imprimir(x, &y);
+    printf("Dentro do main\n");
+    printf("*x: %d\n", x);
+    printf("*y: %d\n", y);
+
+    return 0;
+}
