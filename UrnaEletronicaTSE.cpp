5579e170496792520ab380521bad91aed6ebb539
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-18 00:06:26 -0300

    Update UrnaEletronicaTSE.cpp

--- UrnaEletronicaTSE.cpp
+++ UrnaEletronicaTSE.cpp
@@ -1,37 +1,51 @@
 #include <iostream> 
 #include <locale> 
 using namespace std; 
-
+void menu()
+{
+	cout<<"\n13 - Lula";
+	cout<<"\n39 - Bozo";
+	cout<<"\n1 - Branco";
+	cout<<"\n2 - Nulo";
+	cout<<"\n";
+	cout<<"\n***********";
+	cout<<"\n";
+}
 int main() {
 setlocale(LC_ALL, "ptb");
 
 int cont, num, lula=0, bozo=0, brancos=0, nulos=0;
-
+menu();
+cout<<"\n";
 cout<<"Digite o número do seu candidato: ";
 cin>>num;
 while(num!=0)
 {
 if(num==13)
 {
-cout<<"\nVc votou no Lula";
+cout<<"\nVc votou no LULA";
+cout<<"\n";
 lula+=1;
 cont+=1;
 }
 else if(num==39)
 {
-cout<<"\nVc votou no Bozo";
+cout<<"\nVc votou no BOZO";
+cout<<"\n";
 bozo+=1;
 cont+=1;
 }
 else if (num==1)
 {
-cout<<"\nVc votou em branco";
+cout<<"\nVc votou em BRANCO";
+cout<<"\n";
 brancos+=1;
 cont+=1;
 }
 else
 {
-cout<<"\nVc votou nulo";
+cout<<"\nVc votou NULO";
+cout<<"\n";
 nulos+=1;
 cont+=1;
 }
