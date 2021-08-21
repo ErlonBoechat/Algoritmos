0d7f93ace5750f4c43367d971bf7007104e79931
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-20 23:05:45 -0300

    Update DeclaracaoVariavelFor.cpp

--- DeclaracaoVariavelFor.cpp
+++ DeclaracaoVariavelFor.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     
-    for(int x=0; x<=10; x++)
+    for(int x=0; x<=10; x++) // a variável está declarada dentro da estrutura for, não precisando ser declarada antes
     {
         cout<<x<<"\n";
     }
