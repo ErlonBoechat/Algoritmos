7e77325b69cebb69cf6eaee1f103ab9ffa94eda5
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-20 23:54:25 -0300

    Update MediaTurmaNotas_Passagem_Vetor.cpp

--- MediaTurmaNotas_Passagem_Vetor.cpp
+++ MediaTurmaNotas_Passagem_Vetor.cpp
@@ -5,7 +5,7 @@ double media (double n[], int tam);
 
 int main() {
 
-double notas[5];
+double notas[5];   
 for(int x=0; x<5; x++)
 {
   cout<<"\nNota "<<x+1<<": ";
@@ -21,11 +21,12 @@ system ("pause");
 return 0;
 }
 
-double media (double n[], int tam)
+double media (double n[], int tam) // A função "media" recebe a passagem de um vetor "n" com notas de alunos e a passagem de valor de uma variável inteira "tam" com seu tamanho (quantidade armazenada de valores);
 {
-
-  double soma=0;
-  for (int x=0; x<tam; x++)
-  soma+=n[x];
-  return soma/tam;
+  double soma=0; // Uma variável "soma" é declarada para receber a soma das notas contidas no vetor;
+  for (int x=0; x<tam; x++) // um loop com a quantidade "tam" de repetições (que será passada de "main" para a função "media" - quantidade armazenada de valores) ...
+  {
+  soma+=n[x]; //... vai somar e armazenar todos os valores do vetor "n" na variável "soma";
+  }
+  return soma/tam; // a função vai retornar a divisão de "soma" pela quantidade de valores do vetor, declarada em "tam" (média das notas dos alunos). 
 }
