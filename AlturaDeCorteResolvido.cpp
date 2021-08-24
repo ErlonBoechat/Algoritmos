d20597376d04f25375f4c90b37794f7cb72d8232
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-24 13:25:13 -0300

    Add files via upload

---
+++ AlturaDeCorteResolvido.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int contaMaioresQueH(double [], int tam, double altP);
+
+int main () 
+{
+double alturas[4], procuraAltura;
+
+for(int x=0; x<4; x++)
+  {
+     cout<<"\nAltura "<<x+1<<": ";
+     cin>>alturas[x];  
+  }
+cout<<"\n\nQual a altura minima? ";
+cin>>procuraAltura;
+
+cout<<"\nRelação das Alturas\n";
+for(int x=0; x<4; x++)
+  {
+     cout<<alturas[x]<<"\t";
+     contaMaioresQueH(alturas, 4, procuraAltura);
+     cout<<"\n\nTotal das alturas maiores que 6: "<<contaMaioresQueH(alturas, 4, procuraAltura);
+     cout<<"\n\n";
+  }
+
+system("pause");
+retur 0;
+
+}
+
+int contaMaioresQueH(double alts[], int tam, double altP)
+{
+  int conta=0
+   for(int x=0, t<tam; x++)
+     {
+        if (alts[x]>=altP)
+        conta++;
+     }
+return conta;
+}
