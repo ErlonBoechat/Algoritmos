e66a994fb5d6cec60610ab64ea6acaf5c26a6473
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-24 13:59:24 -0300

    Update AlturaDeCorteResolvido.cpp

--- AlturaDeCorteResolvido.cpp
+++ AlturaDeCorteResolvido.cpp
@@ -5,6 +5,7 @@ int contaMaioresQueH(double [], int tam, double altP);
 
 int main () 
 {
+
 double alturas[4], procuraAltura;
 
 for(int x=0; x<4; x++)
@@ -31,11 +32,13 @@ return 0;
 
 int contaMaioresQueH(double alts[], int tam, double altP)
 {
-  int conta=0;
+int conta=0;
+
    for(int x=0; x<tam; x++)
      {
         if (alts[x]>=altP)
         conta++;
      }
+     
 return conta;
 }
