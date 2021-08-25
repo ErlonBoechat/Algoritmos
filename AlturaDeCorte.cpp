675fb9021c17f6e9dc563cd9b4c3a1acd2571930
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-24 23:16:25 -0300

    Update AlturaDeCorte.cpp

--- AlturaDeCorte.cpp
+++ AlturaDeCorte.cpp
@@ -1,4 +1,4 @@
-// Construa uma função que receba o endereço de um vetor que
+// Construa um algoritmo com uma função que receba o endereço de um vetor que
 // armazena alturas de um grupo de atletas, seu tamanho e a 
 // altura mínima de corte, retornando a quantidade de atletas 
 // com a altura superior ou igual ao corte.
