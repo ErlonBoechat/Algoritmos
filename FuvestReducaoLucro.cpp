0d193c79924e517a08d7cc14bd1f85718760b579
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-27 23:29:06 -0300

    Update FuvestReducaoLucro.cpp

--- FuvestReducaoLucro.cpp
+++ FuvestReducaoLucro.cpp
@@ -1,3 +1,8 @@
+//(FUVEST-SP 2020) A dona de uma lanchonete observou que, vendendo um combo a R$10,00,
+//200 deles são vendidos por dia e que, para cada redição de R$1 nesse preço, ela vende 100
+//combos a mais. Nessas condições, qual é a máxima arrecadação diária que ela espera obter com
+//a venda desse combo? 
+
 #include <iostream>
 using namespace std;
 
