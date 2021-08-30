d271fda17528d85592ccb26a02feff0aa5bfcd9a
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-30 13:04:16 -0300

    Add files via upload

---
+++ Biblioteca.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "minhas.h"
+using namespace std;
+
+int main() {
+
+double notas[5]={6.5,9.2,7.9,8.3,10};
+cout<<"\nRelação das notas originais\n";
+
+for(int x=c; x<5; x++)
+{
+    cout<<notas[x]<<"\t";
+}
+
+    return 0;
+}
