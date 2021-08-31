b237dda0c5bc894d20e732021fe8fccd819cdf90
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-30 23:47:43 -0300

    Update EuTeAmo.cpp

--- EuTeAmo.cpp
+++ EuTeAmo.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int main() {
-for(int x=0; x=1*10^6; x++)
+for(int x=0; x<=1*10^6; x++)
 {
-cout<<"Eu te amo!\t";
+cout<<"Eu te amo!"<<x<<" vezes \t";
 }
 }
