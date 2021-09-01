894f1799d250c5d0fa0ce39d6d8c7c5239f5b1fd
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-31 22:02:57 -0300

    Add files via upload

---
+++ EuTeAmo2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+for(int x=0; x<=0; x++)
+{
+cout<<"Eu te amo "<<x+1<<" vez\n";
+}
+for(int x=1; x<=1*10^6; x++)
+{
+cout<<"Eu te amo "<<x+1<<" vezes\n";    
+}
+}
