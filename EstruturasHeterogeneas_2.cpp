0044d0e140741f7ea3fcf56a255cdd9ea482ecbb
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-08-31 23:41:32 -0300

    Update EstruturasHeterogeneas_2.cpp

--- EstruturasHeterogeneas_2.cpp
+++ EstruturasHeterogeneas_2.cpp
@@ -10,13 +10,13 @@ int main()
     float valor;
   }produto1={"martelo",35.01}, produto2={"furadeira",256.75}, aux;
   
- // if(strcmp(produto1.nomeProd,produto2.nomeProd>0)) // a expressão ">0" retorna o erro: cannot convert 'bool' to 'const char*' 
- // {
+// if(strcmp(produto1.nomeProd,produto2.nomeProd>0))
+  //{
    aux=produto1; produto1=produto2; produto2=aux;
-//  }  
+  //}  
   
   cout<<"\nNome do Produto 1: "<<produto1.nomeProd<<"\t"<<produto1.valor;
-  cout<<"\nNome do Produto 1: "<<produto2.nomeProd<<"\t"<<produto2.valor;
+  cout<<"\nNome do Produto 2: "<<produto2.nomeProd<<"\t"<<produto2.valor;
   cout<<"\n\n"; 
   system("pause");
 }
