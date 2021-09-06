65afa42fd1abc5be33ef8e927e329488fe17d9a7
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-05 23:36:16 -0300

    Add files via upload

---
+++ EstruturasHeterogeneas_7.cpp
@@ -0,0 +1,15 @@
+int main() {
+
+struct prodCad
+{
+ int codigo;
+ float precoCompra, precoVenda;
+};
+
+prodCad produtos[25]; //estoques[25];
+
+produtos[4].codigo={0003};
+produtos[4].precoCompra={1.567};
+produtos[4].precoVenda={2.800};
+  return 0;
+}
