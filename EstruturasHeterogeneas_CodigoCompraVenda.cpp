1d32f67fef71b6cc67460037c102d81e630393ac
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-06 00:15:42 -0300

    Add files via upload

---
+++ EstruturasHeterogeneas_CodigoCompraVenda.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#define TAM 3
+using namespace std;
+
+int main()
+{
+  struct prodCad
+  {
+    int codigo;
+    float precoCompra,precoVenda;    
+  };
+  prodCad produtos[TAM];
+  int x, num;
+  for(x=0; x<TAM; x++)     
+  {
+    cout<<"\nCódigo do Produto 0-10000: ";
+    cin>>num;
+    while(num<0 || num>10000)
+    {
+      cout<<"\nCodigo invalido";
+      cout<<"\nCódigo do Produto 0-10000: ";
+      cin>>num;
+    }
+    produtos[x].codigo=num;
+    cout<<"\nPreco de compra: ";    
+    cin>>produtos[x].precoCompra;
+    produtos[x].precoVenda=produtos[x].precoCompra *1.6;
+    
+  }
+  system("cls");
+  cout<<"\n\nCodigo\tCompra\tVenda\n";  
+  for(x=0; x<TAM; x++)
+  cout<<"\n"<<produtos[x].codigo<<"\t"<<produtos[x].precoCompra<<"\t"<<produtos[x].precoVenda;
+  cout<<"\n\n";
+  system("pause");
+}
