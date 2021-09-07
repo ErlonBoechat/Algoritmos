99a7b82f6a8c4039f1df0d44e278b26a134633cb
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-07 18:31:58 -0300

    Add files via upload

---
+++ EstruturasHeterogeneas_10.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+struct data
+{
+  int dia, mes, ano;
+};
+
+int main ()
+{
+  struct pagto
+  {
+    int codigo;
+    float valor;
+    data venc;
+  }promissorias[2];
+     
+  int x;
+  for(x=0;x<2;x++)
+  {
+    cout<<"\nCodigo: ";
+    cin>>promissorias[x].codigo;
+   
+    cout<<"\nValor a ser pago: ";
+    cin>>promissorias[x].valor;
+    cout<<"\n...............................";
+    cout<<"\nDigite dia: ";
+    cin>>promissorias[x].venc.dia;
+
+    cout<<"\nDigite mes: ";
+    cin>>promissorias[x].venc.mes;
+
+    cout<<"\nDigite ano: ";
+    cin>>promissorias[x].venc.ano;
+    cout<<"\n==============================="; 
+  } 
+  
+  cout<<"\nCodigo\t|Valor\t|Data de vencimento\n";
+  
+  for(x=0;x<2;x++)
+  cout<<"\n  "<<promissorias[x].codigo<<"\t | R$"<<promissorias[x].valor<<"\t | "<<promissorias[x].venc.dia<<"/"<<promissorias[x].venc.mes<<"/"<<promissorias[x].venc.ano;
+  cout<<"\n\n"; 
+}
