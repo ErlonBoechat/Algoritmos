d419054655952e87a937986dc200a83c76797ab0
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-07 17:32:30 -0300

    Update EstruturasHeterogeneas_9.cpp

--- EstruturasHeterogeneas_9.cpp
+++ EstruturasHeterogeneas_9.cpp
@@ -11,10 +11,9 @@ int main ()
   int x,y;
   for(x=0;x<2;x++)
   {
-   
     cout<<"\nPalavra: ";
     cin.getline(palavra[x].pal,21); 
-    }
+  }
  
  palavra[0].pal[0]=toupper(palavra[0].pal[0]);
  palavra[1].pal[0]=toupper(palavra[1].pal[0]);
@@ -23,8 +22,10 @@ int main ()
   for(x=0;x<2;x++)
   {
     for(y=0;palavra[x].pal[y]!='\0';y++)
+    {
       cout<<"\n"<<palavra[x].pal[y];
       cout<<"\n\n";    
+    }
   }
   cout<<"\n\n";
   system("pause");
