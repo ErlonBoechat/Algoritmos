cd45b26aea437d94ff5d1d12e14ee773b4d76a37
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-16 22:33:44 -0300

    Add files via upload

---
+++ ordenacao_e_pesquisa_5_selection_sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void selecao(int vet[], int tam);
+
+int main () {
+
+int x, vet[]={13,23,3,8,1};
+system("cls");
+
+cout<<"Antes da chamada: ";
+
+for(x=0;x<5;x++)
+  cout<<vet[x]<<"\t";
+
+cout<<"\n";
+
+selecao(vet,5);
+
+cout<<"\nDepois da chamada: ";
+
+for(x=0;x<5;x++)
+  cout<<vet[x]<<"\t";
+
+system("pause");
+return(0);
+
+}
+
+void selecao(int vet[], int tam)
+{
+  int j, i, aux, temp;
+  for(i=0;i<tam-1;i++)
+  {
+    aux=i;
+    for(j=i+1;j<tam;j++)
+    {
+      if(vet[aux]>vet[j])
+        aux=j;
+        temp=vet[aux];
+        vet[aux]=vet[i];
+        vet[i]=temp;
+    }
+  }
+}
