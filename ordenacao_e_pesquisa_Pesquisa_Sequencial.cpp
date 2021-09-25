d530f615731353aca16b7f4e1ea9af4a0695f2ae
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-24 22:10:55 -0300

    Update ordenacao_e_pesquisa_Pesquisa_Sequencial.cpp

--- ordenacao_e_pesquisa_Pesquisa_Sequencial.cpp
+++ ordenacao_e_pesquisa_Pesquisa_Sequencial.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #define tam 5
 using namespace std;
-
+void barra();
 int main()
 {
   int L,op,matProcura,pos,achou,matric[]={2,4,5,6,13};
@@ -10,27 +10,34 @@ int main()
   do
   {
     system("cls");
+    barra();
     cout<<"\n1 - Lista";
     cout<<"\n2 - Procura";
-    cout<<"\n3 - Sai";
+    cout<<"\n3 - Sai\n";
+    barra();
     cout<<"\nOpcao: ";
     cin>>op;
 
     switch(op)
     {
-      case 1: cout<<"\nMatriculas\n\n";
+      case 1:
+        barra();
+        cout<<"\nMatriculas\n";
         for(L=0;L<tam;L++)
           cout<<"\n"<<L+1<<" - "<<matric[L];
+          cout<<"\n";
+          barra();
       break;
       
-      case 2: cout<<"\nQual matricula a ser procurada? ";    
+      case 2: cout<<"\nQual matricula a ser procurada? \n";    
       cin>>matProcura;
       achou=0;
         for(L=0;L<tam && achou==0;L++)
         {
           if(matProcura==matric[L])
           {
-            achou=1; pos=L;
+            achou=1; 
+            pos=L;
           }
         }
         if(achou==1)
@@ -46,4 +53,9 @@ int main()
   cout<<"\n\n";
   system:("pause");
   }while(op!=3);
+}
+
+void barra()
+{
+    cout<<"------------";
 }
