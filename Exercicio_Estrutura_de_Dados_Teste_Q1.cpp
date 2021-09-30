0bca475359ca7164a6cf42b254f7b9b8fd87f4c8
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-09-30 00:16:42 -0300

    Update Exercicio_Estrutura_de_Dados_Teste_Q1.cpp

--- Exercicio_Estrutura_de_Dados_Teste_Q1.cpp
+++ Exercicio_Estrutura_de_Dados_Teste_Q1.cpp
@@ -1,38 +1,40 @@
-#include<iostream>
-using namespace std; 
 
 //Questão 1) Considere a seguinte função busca escrita em 
 //linguagem C++ :
 
-
-bool busca(int vetor[ ], int n, int tam) //n==4 //tam==7
-{
-    int ini=0, mid;         //ini==0
-    while (ini <= tam)     //true   
-    {
-         cout << " x ";        //1,       
-         mid = (ini + tam)/2;  //mid==3
-         if (vetor[mid] == n)  //true
-             return true;      //
-         else if (n > vetor[mid])  
-             ini = mid+1;        
-         else
-             tam = mid-1;  
-    }
-    return false;
-}
-
 //Qual a quantidade total de impressões da letra x nas 
 //buscas pelos números n = 4, n = 2 e n = 0 no 
 //vetor [1,2,3,4,5,6,7,8], sendo tam = 7 ?
+
+#include<iostream>
+using namespace std; 
+
+bool busca(int vetor[ ], int n, int tam);
 
 int main() {
 
 int vetor[] = {1,2,3,4,5,6,7,8};
 
 busca(vetor,4,7);
-//busca(vetor,2,7);
-//busca(vetor,0,7);
+busca(vetor,2,7);
+busca(vetor,0,7);
 
   return 0;
+}
+
+bool busca(int vetor[ ], int n, int tam)
+{
+    int ini=0, mid;        
+    while (ini <= tam)     
+    {
+         cout << " x "; 
+         mid = (ini + tam)/2;  
+         if (vetor[mid] == n)
+             return true;    
+         else if (n > vetor[mid])  
+             ini = mid+1;        
+         else
+             tam = mid-1;  
+    }
+    return false;
 }
