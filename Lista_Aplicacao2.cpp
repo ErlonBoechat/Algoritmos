ae3b333e8e7a904399dc9dcdd4316a05288ff169
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-08 21:58:03 -0300

    Update Lista_Aplicacao2.cpp

--- Lista_Aplicacao2.cpp
+++ Lista_Aplicacao2.cpp
@@ -79,7 +79,7 @@ int main()
 void insere(int matric[], int m, int &t, int tamanho)
 {
    if(tamanho==t)
-      cout<<"\nAtenção! Lista cheia\n";
+      cout<<"\nAtencao! Lista cheia\n";
    else
    {
       matric[t]=m;
