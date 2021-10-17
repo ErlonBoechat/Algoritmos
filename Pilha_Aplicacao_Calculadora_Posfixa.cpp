fa738e4685b0332b9c9536e8de575455bef5c7e0
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-17 19:14:15 -0300

    Update Pilha_Aplicacao_Calculadora_Posfixa.cpp

--- Pilha_Aplicacao_Calculadora_Posfixa.cpp
+++ Pilha_Aplicacao_Calculadora_Posfixa.cpp
@@ -3,8 +3,8 @@
 #define TAM 100
 using namespace std;
 
-//void push(float p[], int &t, float v);
-//float pop(float p[]; int &t);
+void push(float p[], int &t, float v);
+float pop(float p[], int &t);
 
 int main()
 {
@@ -21,8 +21,71 @@ int main()
 
    do
    {
+   cout<<"\n";	
    cout<<": ";
-   cin>>s; 
-   }while(t==1000);
+   cin>>s;
+   
+   switch(s[0])
+   {
+      case '+':
+	           a=pop(p,t);
+	           b=pop(p,t);
+	           cout<<"\n"<<a+b<<"\n";
+	           push(p, t, a+b);
+	           break;  
+	  case '-':
+	           a=pop(p,t);
+	           b=pop(p,t);
+	           cout<<"\n"<<b-a<<"\n";
+	           push(p,t,b*a);
+	           break;
+	  case '/':
+	           a=pop(p,t);
+			   b=pop(p,t);
+			   
+			   if(a==0)
+			      cout<<"Divisao por 0";
+			   else
+			   {
+			      cout<<"\n"<<b/a<<"\n";
+			      push(p,t,b/a);
+		       }
+		       break;
+	
+	default: push(p,t, atof(s));           
+   }           
+ }while(s[0]!='s');
+ 
+ system("pause");
 
+}
+
+//insere o elemento na pilha
+void push(float p[], int &t, float v)
+{
+   if(t==TAM-1)
+      cout<<"\nAtencao! Pilha cheia.\n";
+   else
+   {
+      t++; //atualiza o topo	
+      p[t]=v; //pilha recebe valor
+   }
+}
+
+//remove o elemento da pilha
+float pop(float p[], int &t)
+{
+	float v;
+	
+	if(t==-1)
+	{
+	   cout<<"\nAtencao! Pilha vazia";
+	   return 0;
+    }
+    else
+    {
+    	v=p[t];//guarda o valor no topo
+    	t--; //atualiza o topo
+    	return v;
+	}
 }
