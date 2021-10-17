225766d764efb23ee5c4036e374af3991f506523
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-17 17:01:19 -0300

    Add files via upload

---
+++ Pilha_Aplicacao_Calculadora_Posfixa.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+#define TAM 100
+using namespace std;
+
+//void push(float p[], int &t, float v);
+//float pop(float p[]; int &t);
+
+int main()
+{
+   int t=-1;
+   float a, b, p[TAM];
+   char s[10];
+   
+   system("color f1");
+   cout<<"\n***************************************************";
+   cout<<"\n*   Calculadora para quatro operacoes pos-fixa    *";
+   cout<<"\n*         Digite numeros ou operadores            *";
+   cout<<"\n*             Digite s para sair                  *";
+   cout<<"\n***************************************************";
+
+   do
+   {
+   cout<<": ";
+   cin>>s; 
+   }while(t==1000);
+
+}
