f37794f2e735ffda030b7c599f8ade5e8a4d00db
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-16 22:18:13 -0300

    Add files via upload

---
+++ Pilha_Funcao_DESEMEMPILHA.cpp
@@ -0,0 +1,24 @@
+
+                   Desempilhar (Pop)
+
+//implementacao
+resp=desempilha(pilha,topo,val)
+
+if(resp==0)
+   cout<<"\nAtencao, pilha vazia\n";
+else
+   cout<<"\nValor removido: "<<val;
+
+
+//funcao
+int desempilha(int[], int &t, int &v)
+{
+   if(t==-1)
+      return 0;
+   else
+   }
+      v=p[t]; //guarda o valor do topo
+      t--; //atualiza o topo
+      return 1;
+   } 
+}
