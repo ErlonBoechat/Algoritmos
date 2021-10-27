ef3b17a4a0da479d959c01e085d72835285a0ffe
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-26 23:14:57 -0300

    Update Alocacao_Dinamica_Aplicacao_video.cpp

--- Alocacao_Dinamica_Aplicacao_video.cpp
+++ Alocacao_Dinamica_Aplicacao_video.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 #include<locale.h>
 #include<stdlib.h>
+#include<string>
 #include<cstdlib>
+
 using namespace std;
 
 //nó ou nodo
-
 struct nodo
 {
    int info;
    nodo* prox;
 }
+
+nodo* insereFrente(nodo* ptr, int valor);
+nodo* insereFim(nodo* ptr, int valor);
+
+void exibe(nodo* ptr);
+nodo* removeFrente(nodo* ptr);
+nodo* removeFim(nodo* ptr);
+
+void substituiNo(nodo* ptr, int posicao, int novoValor);
+int buscaSequencial(nodo* ptr, int valor);
+void libera(nodo* ptr);
+int contaNos(nodo* ptr);
+
+int main ()
+{
+}   
