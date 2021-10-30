6558c53ae8d3efb1106c5a32fdafd550ea19f23c
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-29 22:20:21 -0300

    Add files via upload

---
+++ Ponteiro_Declaracao.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+   float a=8, *ptr=&a;
+   cout<<"\nConteudo do endereco apontado por ptr: "<<*ptr;
+   cout<<"\n\n";
+   system("pause");
+}
