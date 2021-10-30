1f2c8c9edf95e76c612e5b5342ad5328a9cd742a
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-29 22:40:48 -0300

    Update Ponteiro_Inicializacao_Declaracao2.cpp

--- Ponteiro_Inicializacao_Declaracao2.cpp
+++ Ponteiro_Inicializacao_Declaracao2.cpp
@@ -12,3 +12,16 @@ int main()
    cout<<"\n\n";
    system("pause");
 }
+
+}
+
+//Consideracoes sobre o codigo:
+
+//Foram declaradas duas variáveis do tipo inteira. 
+//A variável foi inicalizada com valor 1989.
+
+//A variável ptrAno é uma variável ponteiro porque tem um asterisco na frente do nome. 
+//Ela apontará para uma variável que armazena um valor inteiro.
+
+//A variável ponteiro foi inicializada. Usa-se o comando de atribuição e se precede 
+//com & o nome da variável que será apontada por ela
