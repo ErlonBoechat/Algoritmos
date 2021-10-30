a4a056f50becc431303c92e9ba4b37f96b4ebad5
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-10-29 23:10:59 -0300

    Update Ponteiro_Inicializacao_Declaracao3.cpp

--- Ponteiro_Inicializacao_Declaracao3.cpp
+++ Ponteiro_Inicializacao_Declaracao3.cpp
@@ -18,8 +18,8 @@ int main()
 
 //ANALISANDO O CÓDIGO
 
-//1)Nas linhas 8 e 9, são exibidos os conteúdos das variáveis a e j. Nada diferente do que conhecemos.
-//2) Na linha 10, é exibido o conteúdo da variável ponteiro pa que, como está apontando para ä variável a, aparece o endereço da variável a.
-//3) Na linha 11, a variável pa aparece com * antes. Isto significa que desejamos o conteúdo do endereço apontado por pa que é o número 13.
-//4) Na linha 12, é exibido o conteúdo da variável ponteiro pj que, como está apontando para ä variável b, aparece o endereço da variável b.
-//5) Na linha 13, a variável pj aparece com * antes. Isto significa que desejamos o conteúdo do endereço apontado por pj que é o número 23.
+//1) Nas linhas 7 e 8, são exibidos os conteúdos das variáveis a e j. 
+//2) Na linha 11, é exibido o conteúdo da variável ponteiro pa que, como está apontando para ä variável a, aparece o endereço da variável a.
+//3) Na linha 12, a variável pa aparece com * antes. Isto significa que desejamos o conteúdo do endereço apontado por pa que é o número 13.
+//4) Na linha 13, é exibido o conteúdo da variável ponteiro pj que, como está apontando para ä variável b, aparece o endereço da variável b.
+//5) Na linha 14, a variável pj aparece com * antes. Isto significa que desejamos o conteúdo do endereço apontado por pj que é o número 23.
