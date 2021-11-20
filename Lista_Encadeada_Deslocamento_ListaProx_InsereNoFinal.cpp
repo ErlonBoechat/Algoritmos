d07a844e305ab4f2a519c01700defdba3e46c3c2
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-11-19 22:50:03 -0300

    Add files via upload

---
+++ Lista_Encadeada_Deslocamento_ListaProx_InsereNoFinal.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+struct nodo {
+   int info;
+   struct nodo* prox; 
+};
+
+int main() {
+  
+  nodo *lista, *link=NULL;
+  
+  lista=new nodo;
+  lista->info=33;
+  lista->prox=link;
+  link=lista;
+  
+  lista=new nodo;
+  lista->info=89;
+  lista->prox=link;
+  link=lista;
+  
+  lista=new nodo;
+  lista->info=71;
+  lista->prox=link;
+  link=lista;
+  
+  lista=new nodo;
+  lista->info=99;
+  lista->prox=link;
+  link=lista;
+  
+  lista=new nodo;
+  lista->info=54;
+  lista->prox=link;
+  link=lista;
+  
+  //while(lista->prox->prox)
+  //{      
+    //lista=lista->prox;
+    nodo *novo=new nodo, *aux;
+  
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+  //} 
+  
+  lista->prox=NULL;
+  
+  return 0;
+}
