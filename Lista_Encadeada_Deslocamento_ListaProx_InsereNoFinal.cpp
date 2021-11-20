690cd8833184fbb8e5bfad726c7383e0b7aa3ff6
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-11-19 23:35:41 -0300

    Update Lista_Encadeada_Deslocamento_ListaProx_InsereNoFinal.cpp

--- Lista_Encadeada_Deslocamento_ListaProx_InsereNoFinal.cpp
+++ Lista_Encadeada_Deslocamento_ListaProx_InsereNoFinal.cpp
@@ -8,40 +8,130 @@ struct nodo {
 
 int main() {
   
+  int count=0;
   nodo *lista, *link=NULL;
   
   lista=new nodo;
   lista->info=33;
   lista->prox=link;
   link=lista;
+  count++;
   
   lista=new nodo;
   lista->info=89;
   lista->prox=link;
   link=lista;
+  count++;
   
   lista=new nodo;
   lista->info=71;
   lista->prox=link;
   link=lista;
+  count++;
   
   lista=new nodo;
   lista->info=99;
   lista->prox=link;
   link=lista;
+  count++;
   
   lista=new nodo;
   lista->info=54;
   lista->prox=link;
   link=lista;
+  count++;
   
- 
+   
+ //Insere no fim
+   
   nodo *novo=new nodo, *aux;
   
     aux=lista;
     while(aux->prox)
     aux=aux->prox;
     aux->prox=novo;
+    novo->info=100;
+    novo->prox=NULL;
+    novo=lista;aux=lista;
+    count++;
+    
+    
+  novo=new nodo, *aux;
   
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+    novo->info=234;
+    novo->prox=NULL;
+    novo=lista;aux=lista;  
+    count++;
+    
+    
+  novo=new nodo, *aux;
+  
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+    novo->info=403;
+    novo->prox=NULL;
+    novo=lista;aux=lista;  
+    count++;
+  
+  novo=new nodo, *aux;
+  
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+    novo->info=200;
+    novo->prox=NULL;
+    novo=lista;aux=lista;  
+    count++;
+    
+  novo=new nodo, *aux;
+  
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+    novo->info=185;
+    novo->prox=NULL;
+    novo=lista;aux=lista; 
+    count++;
+    
+    
+    novo=new nodo, *aux;
+  
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+    novo->info=987;
+    novo->prox=NULL;
+    novo=lista;aux=lista;  
+    count++;
+    
+  novo=new nodo, *aux;
+  
+    aux=lista;
+    while(aux->prox)
+    aux=aux->prox;
+    aux->prox=novo;
+    novo->info=732;
+    novo->prox=NULL;
+    novo=lista;aux=lista; 
+    count++;
+    
+  //Exibe 
+    
+  for(int x=1;x<=count;x++)
+  {
+    cout<<"\n"<<x<<"\t"<<lista->info;
+    lista=lista->prox;
+  }
+  
+  system("pause");
   return 0;
 }
