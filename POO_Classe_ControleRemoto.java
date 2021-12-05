09aa8a53ecf8b02b1d231e62c1a5cbe88e75488c
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-04 23:29:03 -0300

    Update POO_Classe_ControleRemoto.java

--- POO_Classe_ControleRemoto.java
+++ POO_Classe_ControleRemoto.java
@@ -50,35 +50,65 @@ public ControleRemoto()
 
     @Override
     public void abrirMenu() {
+        System.out.println("Está ligado? "+this.getLigado());
+        System.out.println("EStá tocando? "+this.getTocando());
+        System.out.print("Volume: "+this.getVolume());
+        for(int i=0;i<=this.getVolume();i+=10)
+        {
+            System.out.print("|");
+        }
     }
 
     @Override
     public void fecharMenu() {
+        System.out.println("Fechando o menu...");
     }
 
     @Override
     public void maisVolume() {
+        if(this.getLigado())
+        {
+            this.setVolume(this.getVolume()+5);
+        }
     }
 
     @Override
     public void menosVolume() {
+        if(this.getLigado())
+        {
+            this.setVolume(this.getVolume()-5);            
+        }
     }
 
     @Override
     public void ligarMudo() {
+        if(this.getLigado()&& this.getVolume()>0)
+        {
+            this.setVolume(0);
+        }
     }
 
     @Override
     public void desligarMudo() {
+        if(this.getLigado()&&getVolume()==0)
+        {
+            this.setVolume(50);
+        }
     }
 
     @Override
     public void play() {
+        if(this.getLigado()&&!(this.getTocando()))
+        {
+            this.setTocando(true);
+        }
     }
 
     @Override
     public void pause() {
+        if(this.getLigado()==true && this.getTocando())
+        {
+            this.setTocando(false);
+        }
     }
-    
-
 }
