20cb5f90cfc180dc24a4432dc4d2ca79e112afd1
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-06 00:40:17 -0300

    Add files via upload

---
+++ POO_Classe_Lutador_UltraEmojiCombat.java
@@ -0,0 +1,37 @@
+package ultraemojicombat;
+public class Lutador {
+    
+   //atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias, derrotas, empates;
+    
+    //métodos públicos
+public void apresentar()
+{
+    
+}
+public void status()
+{
+    
+}
+public void ganharLuta()
+{
+    
+}
+public void perderLuta()
+{
+    
+}
+public void empatarLuta()
+{
+    
+}
+
+//métodos especiais
+
+}
