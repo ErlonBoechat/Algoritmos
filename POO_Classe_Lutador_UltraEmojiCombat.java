2e0b88c52bab4d9b49ad973d45cf27380d0f4634
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-06 19:11:30 -0300

    Update POO_Classe_Lutador_UltraEmojiCombat.java

--- POO_Classe_Lutador_UltraEmojiCombat.java
+++ POO_Classe_Lutador_UltraEmojiCombat.java
@@ -1,6 +1,5 @@
 package ultraemojicombat;
-public class Lutador {
-    
+public class Lutador {   
    //atributos
     private String nome;
     private String nacionalidade;
@@ -9,6 +8,10 @@ public class Lutador {
     private float peso;
     private String categoria;
     private int vitorias, derrotas, empates;
+
+    Lutador() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     //métodos públicos
 public void apresentar()
@@ -33,5 +36,109 @@ public void empatarLuta()
 }
 
 //métodos especiais
+
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.peso = pe;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        if(peso<52.2)
+        {
+            System.out.println("Categoria inválida. Está abaixo do peso mínimo.");
+        }
+        else if(peso<=70.3)
+        {
+            System.out.println("Categoria: Leve.");
+        }
+        else if(peso<=83.9)
+        {
+            System.out.println("Categoria: Médio.");
+        }
+        else if(peso<=120.2)
+        {
+            System.out.println("Categoria: Pesado.");
+        }
+        else
+        {
+            System.out.println("Categoria inválida: Acima do peso máximo.");
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
 
 }
