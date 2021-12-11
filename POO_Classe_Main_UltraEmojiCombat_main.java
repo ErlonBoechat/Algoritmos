61bbcbe6e48bbc62eea2767dc574b4307122d7cd
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-10 23:46:19 -0300

    Update and rename POO_Classe_Lutador_UltraEmojiCombat_main.java to POO_Classe_Main_UltraEmojiCombat_main.java

--- POO_Classe_Main_UltraEmojiCombat_main.java
+++ POO_Classe_Main_UltraEmojiCombat_main.java
@@ -11,10 +11,8 @@ public class UltraEmojiCombat {
         l[4] = new Lutador("UFOcobol","Brasil",37,1.70f,119.3f,5,4,3);
         l[5] = new Lutador("Nerdaart","EUA",30,1.81f,105.7f,12,2,4);
         
-        
-        l[0].getCategoria();
-        l[0].apresentar();
-        l[0].status();
+    Luta UFC01 = new Luta();   
+    UFC01.marcarLuta(l[4], l[5]);
+    UFC01.lutar();
     }
-    
 }
