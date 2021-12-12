e45e93c95a46ed87da898f674fb7385b25cdf688
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-12 20:06:20 -0300

    Add files via upload

---
+++ POO_Exercicio_Livraria_Interface_Publicacao.java
@@ -0,0 +1,9 @@
+package exerciciopratico;
+public interface Publicacao {
+    //métodos abstratos
+    public void abrir();
+    public void fechar();
+    public void folhear();
+    public void avancarPag();
+    public void voltarPag();
+}
