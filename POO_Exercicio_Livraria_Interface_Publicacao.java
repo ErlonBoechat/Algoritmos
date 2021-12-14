ed5fa6db2b3dc01b4775c9717683688368b168b8
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-13 23:24:20 -0300

    Update POO_Exercicio_Livraria_Interface_Publicacao.java

--- POO_Exercicio_Livraria_Interface_Publicacao.java
+++ POO_Exercicio_Livraria_Interface_Publicacao.java
@@ -3,7 +3,7 @@ public interface Publicacao {
     //métodos abstratos
     public void abrir();
     public void fechar();
-    public void folhear();
+    public void folhear(int p);
     public void avancarPag();
     public void voltarPag();
 }
