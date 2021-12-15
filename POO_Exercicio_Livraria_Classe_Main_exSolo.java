e171e8804fd69326b85c0e1c890ee15825da1789
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-15 00:56:35 -0300

    Update POO_Exercicio_Livraria_Classe_Main_exSolo.java

--- POO_Exercicio_Livraria_Classe_Main_exSolo.java
+++ POO_Exercicio_Livraria_Classe_Main_exSolo.java
@@ -5,5 +5,16 @@ public class ProjetoLivro {
         Pessoa p[] = new Pessoa[2];
         Livro l[] = new Livro[2];
         
+        p[0] = new Pessoa("Erlon",45,"M");
+        p[1] = new Pessoa("Maria",33,"F");
+        
+        l[0] = new Livro("A Origem das Espécies","C. Darwin",502,p[0]);
+        l[1] = new Livro("O Capital: Livro 1","C. Marx",574,p[1]);
+        
+        l[0].fechar();
+        System.out.println(l[0].detalhes());
+        
+        
     }
 }
+ 
