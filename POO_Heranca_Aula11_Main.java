0ad90b8b6191e9e83fb60b2a63880a8bb4e38f22
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2021-12-18 22:53:35 -0300

    Add files via upload

---
+++ POO_Heranca_Aula11_Main.java
@@ -0,0 +1,12 @@
+package aula11;
+public class Aula11 {
+    public static void main(String[] args) {
+        Visitante v1 = new Visitante();
+        //v1.setNome("Erlon");
+        v1.setNome("Erlon");
+        v1.setIdade(45);
+        v1.setSexo("M");
+        v1.toString();
+        System.out.println(v1.toString());
+    }  
+}
