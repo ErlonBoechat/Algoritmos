9aac5f68825b77161f969427dd4d176ac3a6db37
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-20 19:57:46 -0300

    Add files via upload

---
+++ Java_ConversaoTiposImplicita.java
@@ -0,0 +1,22 @@
+/*
+Conversao entre tipos de dados do tipo implicita: Ocorre sem 
+a necessidade do programador interferir. Os valores sao 
+convertidos automaticamente. Isso acontece na conversao de 
+inteiro para real, de numeros para strings ou com o uso de operadores 
+unarios.
+
+*/
+
+class ConversaoTiposExplicita {
+    public static void main(String[] args) {
+
+    float x;
+    int i = 20;
+
+    x = i; // x, que e float, recebe um valor inteiro
+
+    System.out.print(“i = ”+x); 
+    // o programa imprime o valor float 20.0
+    
+   }
+}
