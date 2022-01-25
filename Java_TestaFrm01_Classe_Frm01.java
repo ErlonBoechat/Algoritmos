ac666551f5921c152a95c1b60ac2415af0b5f6cf
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-24 22:23:28 -0300

    Add files via upload

---
+++ Java_TestaFrm01_Classe_Frm01.java
@@ -0,0 +1,24 @@
+
+package estacio;
+
+import javax.swing.JFrame;
+
+public class Frm01 {
+      public void criaTela()
+
+          {
+
+                 JFrame f= new JFrame();
+
+                 f.setSize(290,100);
+
+                 f.setTitle("Cadastro");
+
+                 f.setLocation(10,10);
+
+                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                 f.setVisible(true);
+
+         }
+}
