b8e9d344284fad7351847affc181cbd91b6f7cef
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-25 21:53:39 -0300

    Update Java_TestaFrm01_Classe_Frm01.java

--- Java_TestaFrm01_Classe_Frm01.java
+++ Java_TestaFrm01_Classe_Frm01.java
@@ -1,24 +1,19 @@
-
 package estacio;
 
 import javax.swing.JFrame;
 
 public class Frm01 {
-      public void criaTela()
-
-          {
-
-                 JFrame f= new JFrame();
-
-                 f.setSize(290,100);
-
-                 f.setTitle("Cadastro");
-
-                 f.setLocation(10,10);
-
-                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-                 f.setVisible(true);
-
-         }
+      
+      public void criaTela() 
+      {
+          
+            JFrame f= new JFrame();
+            
+            f.setSize(900,700);
+            f.setTitle("Cadastro");
+            f.setLocation(10,10);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setVisible(true);
+            f.getBackground();
+      }
 }
