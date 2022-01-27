a486b08386a4536ecef30c537e707df3ecdc6477
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-27 20:54:46 -0300

    Update Java_MatApoio_Ex1.java

--- Java_MatApoio_Ex1.java
+++ Java_MatApoio_Ex1.java
@@ -1,3 +1,11 @@
+/*
+Escreva um programa em Java que inicialize duas variáveis inteiras na declaração,
+depois calcule e imprima o resto da divisão da 1ª. pela 2ª. 
+
+Teste se o resto calculado é nulo ou não, emitindo mensagem adequada para ambos 
+os casos.
+*/
+
 class Ex1 {
     public static void main(String[] args) {
         int x=11, y=5;
