9df73dfd8868a4c119f0b11d0e556eb818c536b8
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-27 21:26:48 -0300

    Update Java_MatApoio_Ex2.java

--- Java_MatApoio_Ex2.java
+++ Java_MatApoio_Ex2.java
@@ -1,3 +1,10 @@
+/*
+Escreva um programa em Java que inicialize duas variáveis reais do tipo double,
+calcule e imprima, após mensagens adequadas, o resultado da soma, da subtração, do
+produto e da divisão. Depois, verifique se a soma do primeiro valor com o segundo
+valor é maior ou igual ao dobro do 2º. valor.
+*/
+
 class Ex2 {
     public static void main(String[] args) {
         double x=75.9,y=73.32;
