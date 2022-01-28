54a0f41149ae97d2103fc6f3f902c20c8ac3e3bf
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-27 21:54:17 -0300

    Update Java_MatApoio_Ex3.java

--- Java_MatApoio_Ex3.java
+++ Java_MatApoio_Ex3.java
@@ -1,3 +1,8 @@
+/*
+O que será impresso se fizermos a divisão entre dois valores inteiros e armazenarmos
+o resultado da divisão em uma variável do tipo double ?
+*/
+
 class Ex3 {
     public static void main(String[] args) {
         int x=1345,y=89;
