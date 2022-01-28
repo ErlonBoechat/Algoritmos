be01f0110a23c709d3b8ea464a285375d34b552f
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-27 21:54:32 -0300

    Add files via upload

---
+++ Java_MatApoio_Ex4.java
@@ -0,0 +1,11 @@
+class Ex4 {
+    public static void main(String[] args) {
+        int x=1345;
+        double y=89;
+        double z;
+        
+        z = x/y;
+        
+        System.out.print(z);
+    }
+}
