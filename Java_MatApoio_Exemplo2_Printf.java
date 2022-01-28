26cf7e5808d1d4158e85ee5cfe47bcc2a8a7b84b
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-01-28 17:30:17 -0300

    Add files via upload

---
+++ Java_MatApoio_Exemplo2_Printf.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+   public class Leitura {
+
+      public static void main(String [] args) {
+
+    float media = 34.6f;
+    System.out.printf("Media = %2f ", media);
+   }
+}
