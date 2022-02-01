1da76948075b82b435631b7119b0075ad4e470c2
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-02-01 10:18:44 -0300

    Add files via upload

---
+++ Java_MatApoio_Exemplo4_ConversaoTipos.java
@@ -0,0 +1,10 @@
+class ConversaoTipos {
+    public static void main(String[] args) {
+
+      int i=6, j=3, k=4;
+      float div; 
+      div = (float) i/j;
+      
+      System.out.print(div);
+    }
+}
