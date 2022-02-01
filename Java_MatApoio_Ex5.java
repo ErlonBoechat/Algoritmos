6621b40320ae6bd983190482f1a568bb875dd0f9
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-02-01 11:46:39 -0300

    Add files via upload

---
+++ Java_MatApoio_Ex5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class ResultOps {
+    public static void main(String[] args) {
+
+      int x,y,stored;
+    
+      Scanner numero = new Scanner(System.in);
+      
+      
+         System.out.print("Escreva o primeiro numero: ");
+         x = numero.nextInt();
+         
+         System.out.print("Escreva o segundo numero: ");
+         y = numero.nextInt();
+        
+        System.out.print("\nSoma: " + (x+y) + "\n");
+        System.out.print("Subtracao: " + (x-y) + "\n");
+        System.out.print("Multiplicacao: " + (x*y) + "\n");
+        System.out.print("Divisao: " + (x/y) + "\n");
+        System.out.print("Resto: " + (x%y) + "\n");
+         
+   }
+}
