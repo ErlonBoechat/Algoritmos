69bf340582b7f3e3bf7906801f27ac74b605646f
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-02-10 23:00:48 -0300

    Update Java_MatApoio_Un2_Aula3_Ex2.java

--- Java_MatApoio_Un2_Aula3_Ex2.java
+++ Java_MatApoio_Un2_Aula3_Ex2.java
@@ -40,3 +40,21 @@ public class Livro {
         this.Preco = Preco;
     }
 }
+
+
+public class LivroMain {
+    public static void main(String[] args) {
+       
+        Livro x = new Livro("Autor 1", "Oreilly",79.9f);
+        Livro y = new Livro("Autor 2", "Cambridge",89.9f);
+        
+        System.out.println(x.getAutor());
+        System.out.println(x.getEditora());
+        System.out.println(x.getPreco());
+        
+        System.out.println(y.getAutor());
+        System.out.println(y.getEditora());
+        System.out.println(y.getPreco());
+    }
+    
+}
