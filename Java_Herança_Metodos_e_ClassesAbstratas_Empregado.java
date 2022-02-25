33eb681173906daee77924853d1d90dc710aaf25
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-02-24 22:35:54 -0300

    Update Java_Herança_Metodos_e_ClassesAbstratas_Empregado.java

--- Java_Herança_Metodos_e_ClassesAbstratas_Empregado.java
+++ Java_Herança_Metodos_e_ClassesAbstratas_Empregado.java
@@ -1,3 +1,28 @@
+/*
+-----------------------------------------------------------|
+
+Vamos imaginar a seguinte hierarquia de classes:
+
+                    | Empregado |
+
+ | Chefe | | PorComissão || PorItem || PorHora | 
+
+Onde Chefe, PorComissao, PorItem e PorHora são classes
+finais
+
+-----------------------------------------------------------|
+
+Todas essas subclasses vão precisar redefinir o método
+ganha(). Como se tratam de tipos diferentes de empregado,
+cada um ganha de uma forma: 
+
+
+Exemplo:
+
+//Classe abstrata Empregado
+
+*/
+
 public abstract class Empregado // como a classe contém um metodo abstrato, deve ser declarada como abstrata
 {
    private String nome;
