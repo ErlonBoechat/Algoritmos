d3b80751ca66b07117416730562a4af827da75f5
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-04 23:52:07 -0300

    Add files via upload

---
+++ Java_Herança_Teste_Questao2_Classe1.java
@@ -0,0 +1,17 @@
+class Primeira { 
+    
+    int x = 20;
+    
+    void Calc(int aux1) 
+    { 
+        x*=aux1; 
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
