e354f20cf17a8d970a18e1fa75968dbbeb99e272
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-08 14:32:33 -0300

    Update Java_Metodos_Estaticos_Math_Pow.java

--- Java_Metodos_Estaticos_Math_Pow.java
+++ Java_Metodos_Estaticos_Math_Pow.java
@@ -1,3 +1,12 @@
+/*
+Para calcular qualquer tipo de potências, da forma: a^b
+onde a e b são do tipo double, usamos o método pow() da 
+classe Math
+-----------------------------------------------------------|
+                 numero = Math.pow(a,b)
+-----------------------------------------------------------|
+*/
+
 class StaticMethod {
     public static void main(String[] args) {
         double a=2,b=2, res;
