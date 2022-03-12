0390469eba390e6a2c73604d21294c3d741a8aaa
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-12 00:19:28 -0300

    Update Java_ConstrutorCopia_2_DoisCirculos_Main.java

--- Java_ConstrutorCopia_2_DoisCirculos_Main.java
+++ Java_ConstrutorCopia_2_DoisCirculos_Main.java
@@ -1,21 +1,18 @@
+
 package DoisCirculos;
 
 public class TestaCirculo {
 
     public static void main(String[] args) {
-    Circulo a = new Circulo();
-    Circulo b = new Circulo(5,4);
-   
-    System.out.println("-----------------");
-    System.out.println("Raio do circulo a: "+a.getY());
-    System.out.println("Raio do circulo b: "+b.getY());
-    System.out.println("-----------------");
-    System.out.println("Distancia entre os dois circulos: " + a.distancia(b));
-    System.out.println("-----------------");
-    System.out.println("Área do circulo a: " + a.calcularArea());
-    System.out.println("Área do circulo b: " + b.calcularArea());
-    System.out.println("-----------------");
-   
+       
+        Circulo a = new Circulo();
+        Circulo b = new Circulo(5,4);
+
+        a.imprimirDados();
+        System.out.println("-----------------");
+        b.imprimirDados();
+        System.out.println("-----------------");
+        System.out.println("Distância enrtre a e b: "+ a.distancia(b));
     
     }  
 }
