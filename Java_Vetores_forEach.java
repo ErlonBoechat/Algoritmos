825218623a5f43b9ad02db6545c35df1a47fb57a
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-14 20:28:06 -0300

    Add files via upload

---
+++ Java_Vetores_forEach.java
@@ -0,0 +1,9 @@
+public class Vetor {
+   public static void main(String[] args) {
+      
+      int[] v = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+      for (int n : v)
+         System.out.println(n);
+  }
+}
