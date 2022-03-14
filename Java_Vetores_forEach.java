3f6d2e2fc49a45867a767e517eefc9346e8115ab
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-14 20:30:00 -0300

    Update Java_Vetores_forEach.java

--- Java_Vetores_forEach.java
+++ Java_Vetores_forEach.java
@@ -1,3 +1,12 @@
+/*
+Existe uma outra sintaxe do comando for para percorrer
+vetores. Essa forma é usada apenas para ler os dados do 
+vetor, mas não serve para alterar o vetor. 
+
+Aqui estamos usando o for-each. Não é preciso indexar.
+
+*/
+
 public class Vetor {
    public static void main(String[] args) {
       
