b7c9bf1537337f1b85144cf39baf0c769500b2db
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-15 22:26:05 -0300

    Update Java_Vetores_MatApoio_Ex1.java

--- Java_Vetores_MatApoio_Ex1.java
+++ Java_Vetores_MatApoio_Ex1.java
@@ -10,7 +10,7 @@ public class Vetor {
    public static void main(String[] args) {
 
     int tNotas=0, c=0;
-    float notas[];
+    float notas[],media=0,soma=0;
     
     Scanner tn = new Scanner(System.in);
     Scanner n = new Scanner(System.in);
@@ -27,14 +27,19 @@ public class Vetor {
     {
         System.out.print("Digite a "+(c+1)+"a nota: ");
         notas[c] = n.nextFloat();
+        soma+=notas[c];
     }
-    
     System.out.println("----------------\n");
+    media=soma/tNotas;
+    
+    System.out.println("Media das notas: "+media);
+    System.out.println("----------------\n");
+    System.out.println("Notas acima da media\n");
     
     for(c=0;c<tNotas;c++)
     {
+        if(notas[c]>media)
         System.out.println(notas[c]+"\n");
     }
-    
   }
 }
