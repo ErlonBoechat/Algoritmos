b2d4c4b8644018ef90c2887d9e6538a973cef292
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-24 23:39:53 -0300

    Update Java_AssociacaoDeClasses_Composicao_Classe.java

--- Java_AssociacaoDeClasses_Composicao_Classe.java
+++ Java_AssociacaoDeClasses_Composicao_Classe.java
@@ -1,3 +1,20 @@
+/*
+-----------------------------------------------------------|
+-------------------------------------------- 5 - Composição
+
+Tipo de associação onde uma nova classe usa classes 
+existentes como atributos;
+
+               Relacionamento “tem um”.
+
+Ex.: uma conta tem um dono (cliente),
+     um cliente tem um nome (string);
+
+-----------------------------------------------------------|
+
+*/
+
+
 class Aleatorio {
 
            int numero;
