617ad8061a717149288a4a4a86ce166dc57244cd
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-03-28 22:12:32 -0300

    Add files via upload

---
+++ Java_AssociacaoDeClasses_Composicao_Main.java
@@ -0,0 +1,13 @@
+package PackComposicao;
+
+public class ComposicaoMain {
+
+    public static void main(String[] args) {
+        
+        NumeroAleatorio n;
+
+                     n = new NumeroAleatorio("Número secreto", 50);
+                     
+                     System.out.println(n);
+    }    
+}
