1a0f4ebe2807cbb48d641d9beeca7491e91b90df
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-05-02 23:12:22 -0300

    Add files via upload

---
+++ Java_Listeners_Adapters_MessageDialog.java
@@ -0,0 +1,19 @@
+package exemplo;
+
+import java.awt.Component;
+
+import javax.swing.*;
+
+public class Janela  {
+
+    private static Component janela;
+
+    public static void main(String[] args){
+
+            JOptionPane.showMessageDialog(
+
+            janela, "Cuidado com as Provas!","Linguagem de Programação",JOptionPane.WARNING_MESSAGE);
+
+    }
+
+}
