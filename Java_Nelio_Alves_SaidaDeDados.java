554ad3c2aaf552b5fc85e39a2b79a9a01c047709
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-08-17 01:33:58 -0300

    Update Java_Nelio_Alves_SaidaDeDados.java

--- Java_Nelio_Alves_SaidaDeDados.java
+++ Java_Nelio_Alves_SaidaDeDados.java
@@ -6,17 +6,34 @@ public class SaidaDeDados {
 
 	public static void main(String[] args) {
 
-		System.out.print("Olá, Mundo!"); // o "print" não oferece quebra de linha
 		System.out.println("Olá, Mundo!");
+		
 		
 		// variável de ponto flutuante
 		
 		float x = 3.67456467475467456456f;
 		
 		Locale.setDefault(Locale.US);		
-		System.out.printf("%.2f\n", x);//printf (format). "Porcento, ponto, 2f; porcento n (quebra de linha)"; variável que se quer imprimir
+		System.out.printf("%.1f\n", x);//printf (format). "Porcento, ponto, 2f; porcento n (quebra de linha)"; variável que se quer imprimir
 		                 //padrão de máscara de formatação		
 		System.out.printf("%.2f\n", x);
+		System.out.printf("%.3f\n", x);
+		System.out.printf("%.4f\n", x);
+		System.out.printf("%.5f\n", x);
 		
+		
+		/*
+		
+		output:
+		
+		Olá, Mundo!
+		3.7
+                3.67
+                3.675
+                3.6746
+                3.67456
+
+                */		
+
 	}
 }
