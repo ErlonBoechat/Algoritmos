e017296295613968a3372c59da02dd1337598b2b
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-08-17 01:25:43 -0300

    Update Java_Nelio_Alves_SaidaDeDados.java

--- Java_Nelio_Alves_SaidaDeDados.java
+++ Java_Nelio_Alves_SaidaDeDados.java
@@ -1,5 +1,7 @@
 package Secao4_EstruturaSequencial;
 
+import java.util.Locale;
+
 public class SaidaDeDados {
 
 	public static void main(String[] args) {
@@ -8,11 +10,13 @@ public class SaidaDeDados {
 		System.out.println("Olá, Mundo!");
 		
 		// variável de ponto flutuante
-		float x = 3.67456467475467456456f;
-		System.out.printf("%.2f\n", x);//printf (format). "Porcento, ponto, 2f; porcento n (quebra de linha)"; variável que se quer imprimir
-		                 //padrão de máscara de formatação	
 		
-		// output: 3.67
+		float x = 3.67456467475467456456f;
+		
+		Locale.setDefault(Locale.US);		
+		System.out.printf("%.2f\n", x);//printf (format). "Porcento, ponto, 2f; porcento n (quebra de linha)"; variável que se quer imprimir
+		                 //padrão de máscara de formatação		
+		System.out.printf("%.2f\n", x);
 		
 	}
 }
