67380ad0d11d2b2ad405f599155bd723583d69ac
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-08-16 23:56:55 -0300

    Add files via upload

---
+++ Java_Programação Orientada a Objetos _Nelio_Alves_SaidaDeDados.java
@@ -0,0 +1,18 @@
+package Secao4_EstruturaSequencial;
+
+public class SaidaDeDados {
+
+	public static void main(String[] args) {
+
+		System.out.print("Olá, Mundo!"); // o "print" não oferece quebra de linha
+		System.out.println("Olá, Mundo!");
+		
+		
+		// variável de ponto flutuante
+		
+		float y,x = 3.67456467456f;
+		
+		System.out.printf("%.2f%n", x);
+
+	}
+}
