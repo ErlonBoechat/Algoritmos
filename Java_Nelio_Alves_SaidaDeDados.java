d20bc4bce135e5e8364a4b0b451a2d012aafb287
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-08-20 00:11:03 -0300

    Update Java_Nelio_Alves_SaidaDeDados.java

--- Java_Nelio_Alves_SaidaDeDados.java
+++ Java_Nelio_Alves_SaidaDeDados.java
@@ -27,21 +27,39 @@ public class SaidaDeDados {
 		System.out.println("");
 		System.out.printf("RESULTADO = %.2f metros\n", x);
 		
+		/*		
+                  uso de marcadores para a entrada de valores de variáveis
+		
+                %f = ponto flutuante       
+                %d = inteiro
+                %s = texto
+                %n = quebra de linha
+              
+        */
+		System.out.println("");
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 40000.0;
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n",nome,idade,renda); // as variáveis precisa estar na ordem dos marcadores
+		
+		
 		/*
 		
 		output:
 		
-		Olá, Mundo!
-		3.7
+                Olá, Mundo!
+
+                3.7
                 3.67
                 3.675
                 3.6746
                 3.67456
-		
-		
-		
-		
 
+                RESULTADO = 3.67 metros
+
+                Maria tem 31 anos e ganha R$ 40000.00 reais
+		
+				
                 */		
 
 	}
