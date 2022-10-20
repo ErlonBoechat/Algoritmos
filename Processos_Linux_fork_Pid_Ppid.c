041b14cb8103d731a216082148c2a19091aae75d
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-10-19 22:25:12 -0300

    Update Processos_Linux_fork_Pid_Ppid.c

--- Processos_Linux_fork_Pid_Ppid.c
+++ Processos_Linux_fork_Pid_Ppid.c
@@ -3,17 +3,22 @@
 #include <sys/wait.h>
 
 int main() {
-int resultado, pid, ppid;
-resultado = fork();
-if (resultado < 0)
-printf("Algo deu errado!!!\n");
-pid = getpid();
-if (resultado == 0) {
-ppid = getppid();
-printf("Eu sou o processo filho, meu PID é %d e meu pai tem PID=%d.\n", pid, ppid);
-}
-if (resultado > 0) {
-printf("Eu sou o processo pai, meu PID é %d e meu filho tem PID=%d.\n", pid, resultado);
-waitpid(resultado, NULL, 0);
-}
+   int resultado, pid, ppid;
+
+   resultado = fork();
+
+   if (resultado < 0)
+     printf("Algo deu errado!!!\n");
+
+   pid = getpid();
+
+   if (resultado == 0) {
+     ppid = getppid();
+     printf("Eu sou o processo filho, meu PID é %d e meu pai tem PID=%d.\n", pid, ppid);
+   }
+  
+   if (resultado > 0) {
+     printf("Eu sou o processo pai, meu PID é %d e meu filho tem PID=%d.\n", pid, resultado);
+     waitpid(resultado, NULL, 0);
+   }
 }
