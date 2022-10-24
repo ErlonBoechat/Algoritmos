2327eb603b22f6dcf66dcd90036774f0fb02916d
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-10-23 22:50:26 -0300

    Update Processo_Linux_Subprocesso.c

--- Processo_Linux_Subprocesso.c
+++ Processo_Linux_Subprocesso.c
@@ -21,7 +21,7 @@ void funcao_B(void) {
 
 void main (void){
    int pid;
-   contador = 0 // inicializa o contador
+   contador = 0; // inicializa o contador
    pid = fork(); // processo se divide em dois 
    
    if(pid == 0)
@@ -29,3 +29,79 @@ void main (void){
    else
       funcao_B(); // se PID diferente de zero executa código da função B          
 }
+
+/*
+
+Output (loop infinito):
+
+Função B incrementando contador.
+Para a função B contador = 1.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 2.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 3.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 4.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 5.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 6.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função A contador = 0.
+Para a função B contador = 7.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 8.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 9.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 10.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 11.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 12.
+Função B incrementando contador.
+Para a função A contador = 0.
+Para a função B contador = 13.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 14.
+Função B incrementando contador.
+Para a função A contador = 0.
+Para a função B contador = 15.
+Função B incrementando contador.
+Para a função B contador = 16.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 17.
+Para a função A contador = 0.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 18.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 19.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 20.
+Função B incrementando contador.
+Para a função A contador = 0.
+Para a função B contador = 21.
+Para a função A contador = 0.
+Função B incrementando contador.
+Para a função B contador = 22.
+Função B incrementando contador.
+Para a função B contador = 23.
+Para a função A contador = 0.
+...
+   */
