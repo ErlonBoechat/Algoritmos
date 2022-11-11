4380076053431d091b5e701d7d16923781047e9d
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-11-10 23:34:05 -0300

    Update Processos_Linux_Tratamento_Sinais.c

--- Processos_Linux_Tratamento_Sinais.c
+++ Processos_Linux_Tratamento_Sinais.c
@@ -3,25 +3,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Rotina para tratamento do sinal capturado
-static void captura(int sinal) {
-if (sinal == SIGINT)
-printf("Recebido o sinal SIGINT.\n");
-if (sinal == SIGTERM)
-printf("Recebido o sinal SIGTERM.\n");
-printf("Pretendo continuar executando!!!\n");
-}
+static void captura(int); // Rotina para tratamento do sinal capturado
 
 int main(){
-int i;
-// Captura os sinais SIGINT e SIGTERM
-if ((signal(SIGINT, captura) == SIG_ERR) || (signal(SIGTERM, captura) == SIG_ERR)) {
-printf("Erro ao instalar o tratador do sinais.\n");
-exit(1);
+   int i;
+
+   if ((signal(SIGINT, captura) == SIG_ERR) || (signal(SIGTERM, captura) == SIG_ERR)) {   // Captura os sinais SIGINT e SIGTERM
+      printf("Erro ao instalar o tratador do sinais.\n");
+      exit(1);
+   }
+
+   i = 1;
+
+   while (1) { // Loop infinito
+      printf("Estou vivo [%d].\n", i++);
+      usleep(1000000);
+   }
 }
-i = 1;
-while (1) { // Loop infinito
-printf("Estou vivo [%d].\n", i++);
-usleep(1000000);
-}
+
+static void captura(int sinal) {
+   if (sinal == SIGINT)
+      printf("Recebido o sinal SIGINT.\n");
+   if (sinal == SIGTERM)
+      printf("Recebido o sinal SIGTERM.\n");
+   printf("Pretendo continuar executando!!!\n");
 }
