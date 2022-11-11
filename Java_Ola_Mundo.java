8e654bbe3deb2e29c8ee4e4425a6d21339ee1ce4
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-11-10 23:53:07 -0300

    Update Java_Ola_Mundo.java

--- Java_Ola_Mundo.java
+++ Java_Ola_Mundo.java
@@ -1,6 +1,6 @@
-public class PrimeiroPrograma{
+public class MyFirst{
    public static void main(String args[])
    {
-      System.out.println("Olá, mundo!");   
+      System.out.println("Olá, mundo cão");   
    }
 }
