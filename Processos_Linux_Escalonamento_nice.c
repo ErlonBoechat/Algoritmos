df04525f4af6d03f6ae113e43bfdf375e09fb284
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2022-11-18 00:53:01 -0300

    Update Processos_Linux_Escalonamento_nice.c

--- Processos_Linux_Escalonamento_nice.c
+++ Processos_Linux_Escalonamento_nice.c
@@ -1,7 +1,7 @@
 /*
 O algoritmo será rodado em paralelo com a sua versão 2 (upload seguinte) a partir de dois scripts sh no Linux para que seja
 acompanhado o peso de cada processo (algoritmos 1 e 2) na UCP e para que, com o segundo script, seja definido o nice 5 para o algoritmo 2 (acréscimo de
-5 em sua prioridade). Conferir as diferenças no uso da UCP entre os dois processos com o programa 'top'. 
+5 em sua prioridade). Conferir as diferenças no uso da UCP entre os dois processos com o programa 'top'. Aula 2 Video 4 ARA1386
 */
 
 #include <stdio.h>
