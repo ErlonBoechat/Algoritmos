ad1ac08f9a12ecddd74dd49c97d2cf962f5b4778
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2023-03-30 23:06:20 -0300

    Update Java_Tratamento_Excecoes.java

--- Java_Tratamento_Excecoes.java
+++ Java_Tratamento_Excecoes.java
@@ -19,36 +19,36 @@ public class DivideByZeroWithExceptionHandling {
       int denominator, numerator=0;
 
       while(numerator!=-1) {
-
-         do {
-
+                 
+         do {   
+            
+            System.out.print("\n\nPlease, enter an integer numerator: ");
+            numerator = scanner.nextInt();
+             
+            System.out.print("Please, enter an integer denominator: ");          
+            denominator = scanner.nextInt();
+             
             try {
-
-               System.out.print("\n\nPlease, enter an integer numerator: ");
-               numerator = scanner.nextInt();
-
-               System.out.print("Please, enter an integer denominator: ");          
-               denominator = scanner.nextInt();
             
                int result = quotient(numerator,denominator); 
                System.out.printf("%nRESULT: %d/%d = %d%n\n", numerator, denominator, result);
                line();
                continueLoop = false;
-
+             
             } catch (InputMismatchException inputMismatchException) {
-
-                 System.err.printf("%nException: %s%n", inputMismatchException);
-                 scanner.nextLine(); // descarta a entrada para o usuário tentar de novo
-                 System.out.printf("You must enter integers. Please try again.%n%n");
-                 line();
-
-              } catch (ArithmeticException arithmeticException) {
-
-                   System.err.printf("%nException: %s%n", arithmeticException);
-                   System.out.printf("Zero is an invalid denominator. Please try again.%n%n");
-                   line();                
-                }
-
+                       
+               System.err.printf("%nException: %s%n", inputMismatchException);
+               scanner.nextLine(); // descarta a entrada para o usuário tentar de novo
+               System.out.printf("You must enter integers. Please try again.%n%n");
+               line();
+                   
+            } catch (ArithmeticException arithmeticException) {
+                  
+               System.err.printf("%nException: %s%n", arithmeticException);
+               System.out.printf("Zero is an invalid denominator. Please try again.%n%n");
+               line();                
+            }
+             
          } while(continueLoop);  
       }
    }
