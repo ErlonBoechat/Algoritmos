384eec6d09b847bd17f07f1813eada9badad1db9
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2023-03-30 23:24:54 -0300

    Update Java_Tratamento_Excecoes.java

--- Java_Tratamento_Excecoes.java
+++ Java_Tratamento_Excecoes.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class DivideByZeroWithExceptionHandling {
 
-   public static int quotient(int numerator, int denominator) throws ArithmeticException {
+   public static int quotient(int numerator, int denominator) throws ArithmeticException,InputMismatchException {
       return numerator/denominator;  
    }
 
@@ -22,13 +22,13 @@ public class DivideByZeroWithExceptionHandling {
                  
          do {   
             
-            System.out.print("\n\nPlease, enter an integer numerator: ");
-            numerator = scanner.nextInt();
-             
-            System.out.print("Please, enter an integer denominator: ");          
-            denominator = scanner.nextInt();
-             
             try {
+                 
+               System.out.print("\n\nPlease, enter an integer numerator: ");
+               numerator = scanner.nextInt();
+             
+               System.out.print("Please, enter an integer denominator: ");          
+               denominator = scanner.nextInt();           
             
                int result = quotient(numerator,denominator); 
                System.out.printf("%nRESULT: %d/%d = %d%n\n", numerator, denominator, result);
