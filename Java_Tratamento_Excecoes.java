636c5c6d9d557ce3bca45262704f44f148c7f61c
Erlon Boechat <ErlonBoechat@users.noreply.github.com>
2023-03-30 23:27:44 -0300

    Update Java_Tratamento_Excecoes.java

--- Java_Tratamento_Excecoes.java
+++ Java_Tratamento_Excecoes.java
@@ -21,15 +21,15 @@ public class DivideByZeroWithExceptionHandling {
       while(numerator!=-1) {
                  
          do {   
-            
+                
             try {
                  
                System.out.print("\n\nPlease, enter an integer numerator: ");
                numerator = scanner.nextInt();
-             
+                 
                System.out.print("Please, enter an integer denominator: ");          
                denominator = scanner.nextInt();           
-            
+                
                int result = quotient(numerator,denominator); 
                System.out.printf("%nRESULT: %d/%d = %d%n\n", numerator, denominator, result);
                line();
